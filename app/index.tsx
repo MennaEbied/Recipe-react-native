2eadb523d81d7e3f184571766b4f31f199429b3e
MennaEbied <mennahesham253@gmail.com>
2024-10-26 21:59:47 +0300

    Set up bottom tabs

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View  } from 'react-native';
+import { LogoImage } from '../components/logoImage';
+import { theme } from '../theme';
+
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <LogoImage/>
+     <Text style={styles.text}>Foody</Text>
+     <Text style={{fontSize:30,color:theme.colorWhite}}>Food is always right</Text>
     </View>
   );
 }
@@ -13,8 +18,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent:"center",
+    alignItems:"center",
+    backgroundColor:theme.colorOrange,
   },
+  text:{
+    fontSize:35,
+    fontWeight:"bold",
+    textAlign:"center",
+    justifyContent:"center",
+    alignItems:"center",
+    color:theme.colorWhite,
+
+  },
+  backGround:{
+    flex:1,
+  }
 });
