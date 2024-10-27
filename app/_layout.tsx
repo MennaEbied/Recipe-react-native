f05e6f6c0e7e763c3c18f14de29aed8a83590c65
MennaEbied <mennahesham253@gmail.com>
2024-10-27 20:19:24 +0300

    Set up navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,13 @@
-import { Tabs } from "expo-router";
-import Entypo from '@expo/vector-icons/Entypo';
-import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
-import { theme } from "../theme";
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { Stack } from 'expo-router';
 
-export default function Layout(){
-    return(
-        <Tabs screenOptions={{ tabBarActiveTintColor: theme.colorBlack }}>
-            <Tabs.Screen 
-             name="index"
-             options={{
-                 title: "Home",
-                 tabBarIcon:({size,color})=>(
-                    <Entypo name="home" size={size} color={color} />
-                 )
-               }}
-             />
-            <Tabs.Screen 
-             name="recipes" 
-             options={{ 
-                title: "Recipes",
-                tabBarIcon:({size,color})=>(
-                    <FontAwesome6 name="bowl-food" size={size} color={color} /> 
-                )
-               }}
-             />
-            <Tabs.Screen 
-             name="favourite" 
-             options={{ 
-                title: "Favourite",
-                tabBarIcon:({size,color})=>(
-                    <MaterialIcons name="favorite" size={size} color={color} />
-                )
-               }}
-             />
-        </Tabs>
-    )
+export default function Layout() {
+  return (
+     <Stack>
+        <Stack.Screen
+        name='index' 
+        options={{
+            headerShown:false,animation:"fade"
+        }}/>
+      </Stack>
+  )
 }
