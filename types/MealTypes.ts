20217d54dbe256605a3f16efaeda81c5ea1294ba
MennaEbied <mennahesham253@gmail.com>
2024-10-27 20:16:14 +0300

    Define types for meals and categories

---
+++ types/MealTypes.ts
@@ -0,0 +1,14 @@
+export interface MealCategory {
+    idCategory: string;
+    strCategory: string;
+    strCategoryThumb: string;
+    strCategoryDescription: string;
+  }
+  
+  export interface Meal {
+    idMeal: string;
+    strMeal: string;
+    strInstructions: string;
+    strMealThumb: string;
+  }
+  
