7d3ba3f771eb7dd993d435357b75aca6dc2c80da
MennaEbied <mennahesham253@gmail.com>
2024-10-28 21:17:41 +0300

    Add header icon

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,6 @@
-import { Stack } from 'expo-router';
+import { Stack,Link } from 'expo-router';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import { Pressable } from 'react-native';
 
 export default function Layout() {
   return (
@@ -7,6 +9,46 @@ export default function Layout() {
         name='index' 
         options={{
             headerShown:false,animation:"fade"
+        }}/>
+        <Stack.Screen
+        name='categories/index' 
+        options={{
+            animation:"fade",
+            title:"Categories",
+            headerLeft:()=>(
+              <Link href="/" asChild>
+              <Pressable hitSlop={20} style={{ marginRight: 18 }}>
+                <AntDesign name="back" size={24} color="black" style={{marginLeft:6}} />
+              </Pressable>
+              </Link>
+             )   
+        }}/>
+
+        <Stack.Screen
+        name='recipe/[category]' 
+        options={{
+           animation:"fade",
+            title:"Recipes",
+           headerLeft:()=>(
+            <Link href="/categories" asChild>
+            <Pressable hitSlop={20} style={{ marginRight: 18 }}>
+              <AntDesign name="back" size={24} color="black" style={{marginLeft:6}} />
+            </Pressable>
+            </Link>
+           )   
+        }}/>
+        <Stack.Screen
+        name='recipe/[category]/[recipeId]' 
+        options={{
+           animation:"fade",
+            title:"Instructions",
+           headerLeft:()=>(
+            <Link href="./" asChild>
+            <Pressable hitSlop={20} style={{ marginRight: 18 }}>
+              <AntDesign name="back" size={24} color="black" style={{marginLeft:6}} />
+            </Pressable>
+            </Link>
+           )   
         }}/>
       </Stack>
   )
